43312e62e40934300273cbe7ac7e9b5ed1efea1e
Dakotah-S <dksingh6381@gmail.com>
2024-03-21 12:13:31 -0500

    I started the basic front page by adding the Navbar, the header along with the cards and place holder images

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bookstore</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="./assets/css/style.css" rel="stylesheet" />
+  </head>
+
+
+    <body>
+        <?php include"./assets/navbar.php" ?>
+        <?php include"./assets/header.php" ?>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+        <div class="container px-4 px-lg-5 mt-5">
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-lx-4 justify-content-center">
+
+                
+                <div class="col md-5">
+                    <div class="card h-100" style="width: 18rem;">
+                        <img src="./assets/image/450_300.jpg" class="card-img-top" alt="...">
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <h5 class="fw-bolder">Horse betting pdf</h5>
+                                "$40.00 - $80.00"
+                            </div>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>       
+                        </div>
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="text-center">
+                                <a href="#" class="btn btn-outline-dark">Add To Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="col md-5">
+                    <div class="card h-100" style="width: 18rem;">
+                        <img src="./assets/image/450_300.jpg" class="card-img-top" alt="...">
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <h5 class="fw-bolder">Dog betting pdf</h5>
+                                "$40.00 - $80.00"
+                            </div>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>       
+                        </div>
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="text-center">
+                                <a href="#" class="btn btn-outline-dark">Add To Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col md-5">
+                    <div class="card h-100" style="width: 18rem;">
+                        <img src="./assets/image/450_300.jpg" class="card-img-top" alt="...">
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <h5 class="fw-bolder">Something betting pdf</h5>
+                                "$40.00 - $80.00"
+                            </div>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>       
+                        </div>
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="text-center">
+                                <a href="#" class="btn btn-outline-dark">Add To Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+
+
+    </body>
+</html>
