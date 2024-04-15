129cd282a1665f6a5b869627a40d2fdc49cc11ab
Dakotah-S <dksingh6381@gmail.com>
2024-04-15 15:03:35 -0500

    i created the sidebar

---
+++ assets/sidebar.php
@@ -0,0 +1,84 @@
+<style>
+.sidebar-content {
+  width: 200px;
+  background-color: #f5f5f5;
+  padding: 10px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.sidebar-content ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.sidebar-content ul li {
+  padding: 5px 0;
+  border-bottom: 1px solid #ddd;
+}
+
+.sidebar-content ul li:last-child {
+  border-bottom: none;
+}
+
+#toggleSidebarBtn {
+  color: blue;
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+</style>
+</head>
+<body>
+<!-- Button to toggle sidebar -->
+<a href="#" id="toggleSidebarBtn">Show Categories</a>
+
+<!-- Content -->
+<div class="sidebar-content">
+  <!-- Categories will be added here dynamically -->
+</div>
+
+
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+  const toggleSidebarBtn = document.getElementById('toggleSidebarBtn');
+  const sidebarContent = document.querySelector('.sidebar-content');
+  let sidebarOpen = false;
+
+  toggleSidebarBtn.addEventListener('click', function() {
+    if (sidebarOpen) {
+      // Close sidebar
+      sidebarContent.innerHTML = '';
+      sidebarContent.style.width = '0';
+      sidebarOpen = false;
+    } else {
+      // Open sidebar and add categories
+      sidebarContent.innerHTML = '';
+      const categories = [
+        { name: 'Travel', url: '/travel' },
+        { name: 'Finance', url: '/finance' },
+        { name: 'Stocks', url: '/stocks' },
+        { name: 'Real Estate', url: '/real-estate' }
+      ];
+      const categoriesList = document.createElement('ul');
+      categories.forEach(category => {
+        const listItem = document.createElement('li');
+        const categoryLink = document.createElement('a');
+        categoryLink.href = category.url;
+        categoryLink.textContent = category.name;
+        listItem.appendChild(categoryLink);
+        categoriesList.appendChild(listItem);
+      });
+      sidebarContent.appendChild(categoriesList);
+      sidebarContent.style.width = '200px'; // Adjust width as needed
+      sidebarOpen = true;
+    }
+  });
+});
+
+</script>
+
+
+  <!-- Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
--- index.php
+++ index.php
@@ -21,8 +21,9 @@ if(isset($_SESSION['cart'])) {
   <link href="./assets/css/style.css" rel="stylesheet" />
 </head>
 <body>
-        
-                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div id="contentContainer">
+        <!--Navbar start-->
+            <nav class="navbar navbar-expand-lg bg-body-tertiary" id="navbar">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="index.php">Bookstore</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,24 +34,6 @@ if(isset($_SESSION['cart'])) {
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Features</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown link
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
                     <!-- Shopping Cart -->
                     <div class="navbar-text ms-auto">
                         <a href="./assets/cart.php" class="btn btn-outline-dark">
@@ -62,36 +45,46 @@ if(isset($_SESSION['cart'])) {
                     </div>
                 </div>
             </nav>
+        <!--Navbar end-->
 
         <?php include"./assets/hero.php" ?>
+    
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-        <div class="container">
+        <div class="container-fluid">
         <h1 class="mt-5 mb-4"></h1>
         <div class="row">
-            <?php foreach ($books as $book): ?>
-            <div class="col-md-4">
-                <div class="card h-100" style="width: 18rem;">
-                    <img src="./assets/image/<?php echo $book['image']; ?>" class="card-img-top" alt="...">
-                    <div class="card-body p-4">
-                        <div class="text-center">
-                            <h5 class="fw-bolder"><?php echo $book['title']; ?></h5>
-                            <?php echo "$" . $book['price']; ?>
+            <div class="col-md-2">
+            <?php include"./assets/sidebar.php"?>
+            </div>
+                <div class="col-md-10">
+                    <div class="row">
+                    <?php foreach ($books as $book): ?>
+                    <div class="col-md-4 mb-3" align="center">
+                        <div class="card h-100" style="width: 18rem;">
+                            <img src="./assets/image/<?php echo $book['image']; ?>" class="card-img-top" alt="...">
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder"><?php echo $book['title']; ?></h5>
+                                    <?php echo "$" . $book['price']; ?>
+                                </div>
+                                <p class="card-text"><?php echo $book['description']; ?></p>       
+                            </div>
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center">
+                                    <a href="./assets/cart.php?action=add&id=<?php echo $book['id']; ?>" class="btn btn-outline-dark">Add to cart</a>
+                                    <a href="./views/product-page.php?id=<?php echo $book['id']; ?>" class="btn btn-outline-dark">View Details</a>
+                                </div>
+                            </div>
                         </div>
-                        <p class="card-text"><?php echo $book['description']; ?></p>       
                     </div>
-                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                        <div class="text-center">
-                            <a href="./assets/cart.php?action=add&id=<?php echo $book['id']; ?>" class="btn btn-outline-dark">Add to cart</a>
-                            <a href="./views/product-page.php?id=<?php echo $book['id']; ?>" class="btn btn-outline-dark">View Details</a>
-                        </div>
+                    <?php endforeach; ?>
                     </div>
                 </div>
-            </div>
-            <?php endforeach; ?>
         </div>
     </div>
+    </div>
         <?php include"./assets/footer.php"?>
-    </body>
+</body>
 </html>
